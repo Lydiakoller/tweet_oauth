f1ab3f77f3c14d90c993fc26125bd58af14741af
Brian Murphy <bmurphy1@mac.com>
2014-02-11 19:28:07 -0600

    added oauth for users

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $("#tweet_form").submit(function(e){
+    e.preventDefault();
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    var text = $("#tweet").val();
+    var tweet = {tweet_text: text};
+    beforeLoad();
+    $.ajax({
+      type: "POST",
+      url: "/send_tweet",
+      data: tweet,
+      success: afterLoad()
+    });
+    function afterLoad(){
+      $('#tweet').prop('disabled', false);
+      $("#ajax_loader").remove();
+      $("<p>Success!</p>").appendTo("#success");
+    }
+    function beforeLoad(){
+      $("#tweet").prop('disabled', true);
+      $("<img src='ajax-loader.gif'/>").appendTo("#ajax_loader");
+    }
+  });
 });
